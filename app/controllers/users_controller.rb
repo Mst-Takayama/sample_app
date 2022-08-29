3aa7020c7bfc468177712ba0cc882a4bb78b3c83
Masato Takayama <masato.takayama@leber.jp>
2022-08-29 22:44:08 +0900

    finish message delivary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,19 +15,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    def destroy
-      User.find(params[:id]).destroy
-      flash[:success] = 'User deleted'
-      redirect_to users_url
-    end
+    User.find(params[:id]).destroy
+    flash[:success] = 'User deleted'
+    redirect_to users_url
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to root_url
     else
       render 'new'
     end
